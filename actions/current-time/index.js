69c21ff1beedf6416e8d38bdce6e3d8ab469c3ed
Thomas Dupoiron <tdupoiron@github.com>
2022-10-24 18:21:22 +0200

    removed input

--- actions/current-time/index.js
+++ actions/current-time/index.js
@@ -1,7 +1,3 @@
-import dateFormat, { masks } from "dateformat";
-import core from "@actions/core";
+const core = require('@actions/core');
 
-const now = new Date();
-const format = core.getInput('format');
-
-core.setOutput("time", dateFormat(now, format));
+core.setOutput("time", new Date().toLocaleString());
