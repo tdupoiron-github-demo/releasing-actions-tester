59d68415f7f317c6d707dbde2ee9b04333a6add9
Thomas Dupoiron <tdupoiron@github.com>
2022-10-25 16:45:28 +0200

    feat: Add debug messages in console

--- actions/dice-roll/index.js
+++ actions/dice-roll/index.js
@@ -2,4 +2,6 @@ const core = require('@actions/core');
 
 const sides = core.getInput("sides");
 
+console.log("Number of sides: ", sides);
+
 core.setOutput("roll", Math.floor(Math.random() * sides) + 1);
