61ac78473198f4b89a72000e64502ed9c7ed2ab1
Thomas Dupoiron <tdupoiron@github.com>
2022-10-25 16:53:45 +0200

    fix: Review debug message

--- actions/dice-roll/index.js
+++ actions/dice-roll/index.js
@@ -2,6 +2,6 @@ const core = require('@actions/core');
 
 const sides = core.getInput("sides");
 
-console.log("Number of sides: ", sides);
+console.log("Number sides: ", sides);
 
 core.setOutput("roll", Math.floor(Math.random() * sides) + 1);
